be6dbaca2033780bba21c94389d66f54aba32a0b
Harrey Jia <jlx19980823@gmail.com>
2019-06-11 12:04:42 +0800

    Build the welcome swift file and implement the jump function. Ref #14

--- HuaRongRoad/userHome.swift
+++ HuaRongRoad/userHome.swift
@@ -1,25 +1,26 @@
 //
-//  ViewController.swift
+//  userHome.swift
 //  HuaRongRoad
 //
-//  Created by Harrey Jia on 2019/5/1.
+//  Created by Harrey Jia on 2019/6/11.
 //  Copyright © 2019 Harrey Jia. All rights reserved.
 //
+
 import UIKit
 
-class ViewController: UIViewController {
-
+class userHome: UIViewController {
     
-    @IBOutlet weak var userLogin: UIButton!
+    @IBOutlet weak var startGameButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func goUserHome(_ sender: Any) {
+    
+    @IBAction func gameDidStart(_ sender: UIButton) {
         let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-        let homeVC = storyBoard.instantiateViewController(withIdentifier: "Home")
+        let homeVC = storyBoard.instantiateViewController(withIdentifier: "GameDetail")
         self.present(homeVC, animated: true, completion: nil)
     }
 }
