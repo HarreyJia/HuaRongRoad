b9650c0027164667dc5d2ffa107f460b53ba8e12
Harrey Jia <jlx19980823@gmail.com>
2019-06-12 13:49:23 +0800

    Add the introduction. Ref #16

--- HuaRongRoad/userHome.swift
+++ HuaRongRoad/userHome.swift
@@ -12,11 +12,19 @@ class userHome: UIViewController {
     
     @IBOutlet weak var startGameButton: UIButton!
     @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var introText: UILabel!
     
     var nickName = String()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        introText.lineBreakMode = NSLineBreakMode.byWordWrapping
+        
+        introText.numberOfLines = 0
+        
+        introText.text = "         The SpongeBob Escape is developed by Jia Lingxiang, a student at Nankai University's School of Software. It is based on the improvement of the Three Kingdoms Huarong Road, using the classic image of SpongeBob to play games for childhood. The basic gameplay is similar to that of the Three Kingdoms Huarong Road. You only need the SpongeBob to reach the exit."
+        
         
         userName.text = UserDefaults().string(forKey: "userName") ?? ""
     }
